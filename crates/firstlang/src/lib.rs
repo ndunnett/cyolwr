a017716c097e4a4e125f5c0c8b922e6715be315c
ndunnett <nathandunnett@me.com>
2026-01-08 08:44:23 +0000

    add anyhow

--- crates/firstlang/src/lib.rs
+++ crates/firstlang/src/lib.rs
@@ -2,9 +2,12 @@ mod ast;
 mod interpreter;
 mod parser;
 
+pub type Anyhow<T> = anyhow::Result<T>;
+pub use anyhow::anyhow;
+
 pub use interpreter::Value;
 
-pub fn run(source: &str) -> Result<Value, String> {
+pub fn run(source: &str) -> Anyhow<Value> {
     println!("{source}");
     Ok(Value::Unit)
 }
