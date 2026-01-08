6d0af61cc83cf49d708c310c7d3bdc7ba75f5abf
ndunnett <nathandunnett@me.com>
2026-01-08 08:44:39 +0000

    add Display trait to Value

--- crates/firstlang/src/interpreter.rs
+++ crates/firstlang/src/interpreter.rs
@@ -4,3 +4,13 @@ pub enum Value {
     Bool(bool),
     Unit,
 }
+
+impl std::fmt::Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Int(n) => write!(f, "{n}"),
+            Self::Bool(b) => write!(f, "{b}"),
+            Self::Unit => write!(f, "()"),
+        }
+    }
+}
